db537c64b1663cd9d5c657763be293f7f303ef98
Nipun <nipun1969@gmail.com>
2024-04-27 13:54:36 +0530

    Fix datetime import

--- models/accesspoint.py
+++ models/accesspoint.py
@@ -1,11 +1,12 @@
 import mongoengine as me
+import datetime
 
 class AccessPoint(me.Document):
     projectId = me.StringField(required=True)
     ssid = me.StringField(required=True)
     bssid = me.StringField(required=True)
-    created_at = me.DateTimeField(default=me.datetime.utcnow)
-    updated_at = me.DateTimeField(default=me.datetime.utcnow)
+    created_at = me.DateTimeField(default=datetime.datetime.now)
+    updated_at = me.DateTimeField(default=datetime.datetime.now)
 
     def __str__(self):
         return f"{self.ssid} ({self.bssid})"
--- models/calibrationFingerprint.py
+++ models/calibrationFingerprint.py
@@ -1,12 +1,13 @@
 import mongoengine as me
+import datetime
 
 class CalibrationFingerprint(me.Document):
     id = me.StringField(required=True)
     projectId = me.StringField(required=True)
     calibrationPointId = me.StringField(required=True)
     radioMap = me.MapField(field=me.FloatField())
-    created_at = me.DateTimeField(default=me.datetime.utcnow)
-    updated_at = me.DateTimeField(default=me.datetime.utcnow)
+    created_at = me.DateTimeField(default=datetime.datetime.now)
+    updated_at = me.DateTimeField(default=datetime.datetime.now)
 
     def __str__(self):
         return f"CalibrationFingerprint(id={self.id}, projectId={self.projectId})"
