54af5098227db64b65c15e61654f78fac8d31408
Nipun <nipun1969@gmail.com>
2024-04-27 20:04:39 +0530

    Add shortest path logic

--- services/navigation.py
+++ services/navigation.py
@@ -1,10 +1,3 @@
-door_to_loc_distance = 0.5
-
-graph2 = {
-    'Seminar_Room_Door': [['L_Seminar_Room', door_to_loc_distance], ['J1', 4], ['E-wis_Lab_Door', 3], ['CIT_Door', 7]],
-    'CIT_Door': [['L_CIT', door_to_loc_distance], ['Studio_Door', 10]]
-}
-
 def heuristic(node, goal):
     return 0
 
@@ -38,7 +31,7 @@ def a_star(graph, start, goal):
             while current != start:
                 current = came_from[current]
                 path.insert(0, current)
-            return path
+            return path, f_score[goal]
 
         open_set.remove(current)
         closed_set.add(current)
@@ -61,67 +54,95 @@ def a_star(graph, start, goal):
 
     return None  # If there is no path
 
-graph = {
-    'A': [['Final_Year_Lab', 1], ['B', 30], ['U', 46]],
-    'B': [['SE', 1], ['Fabric_Lab', 1], ['C', 74], ['A', 30]],
-    'C': [['Washroom', 1], ['Embedded_Enginnering_Lab', 1], ['D', 44], ['B', 74]],
-    'D': [['Research_lab', 1], ['E', 80], ['C', 44]],
-    'E': [['Old_Advanced_Lab', 1], ['F', 38], ['D', 80]],
-    'F': [['RA_Lab', 1], ['G', 78], ['E', 38]],
-    'G': [['A1', 1], ['F', 78], ['H', 44]],
-    'H': [['G', 44], ['I', 78], ['W', 36]],
-    'I': [['A2', 1], ['H', 78], ['J', 50]],
-    'J': [['Systems_Lab', 1], ['ICE_Room', 1], ['I', 50], ['K', 28]],
-    'K': [['J', 28], ['L', 36], ['Staff_Washroom', 1]],
-    'L': [['K', 36], ['M', 52], ['Old_Codegen_Lab', 1]],
-    'M': [['L', 52], ['N', 36], ['A3', 1], ['Seminar_Room', 1], ['E_Wis_Lab', 1]],
-    'N': [['M', 36], ['O', 72], ['Codegen_Lab', 1]],
-    'O': [['N', 72], ['P', 18], ['Sysco_Lounge', 1]],
-    'P': [['O', 18], ['Q', 34], ['V', 42], ['Insight_Hub', 30]],
-    'Q': [['P', 34], ['R', 62], ['Insight_Hub', 30]],
-    'R': [['Q', 62], ['S', 36], ['A4', 1]],
-    'S': [['R', 36], ['T', 36], ['HPC_Lab', 1]],
-    'T': [['S', 36], ['U', 70], ['IntelliSense_Lab', 1]],
-    'U': [['T', 70], ['A', 46], ['L1_Lab', 1]],
-    'V': [['P', 42], ['W', 28], ['CSE_Office', 1]],
-    'W': [['V', 28], ['H', 36], ['HoD_Office', 1]],
-    'A1': [['G', 1], ['Staff_Rooms', 44]],
-    'A2': [['I', 1], ['Staff_Rooms', 78], ['Conference_Room', 1], ['Lunch_Room', 1]],
-    'A3': [['M', 1], ['Studio', 1]],
-    'A4': [['R', 1], ['Embedded_Lab', 1], ['Network_Lab', 1]],
-    'Final_Year_Lab': [['A', 1]],
-    'SE': [['B', 1]],
-    'Fabric_Lab': [['B', 1]],
-    'Washroom': [['C', 1]],
-    'Embedded_Enginnering_Lab': [['C', 1]],
-    'Research_lab': [['D', 1]],
-    'Old_Advanced_Lab': [['E', 1]],
-    'RA_Lab': [['F', 1]],
-    'Staff_Rooms': [['A1', 44], ['A2', 78]],
-    'Conference_Room': [['A2', 1]],
-    'Lunch_Room': [['A2', 1]],
-    'Systems_Lab': [['J', 1]],
-    'ICE_Room': [['J', 1], ['Server_Room', 1]],
-    'Server_Room': [['ICE_Room', 1]],
-    'Staff_Washroom': [['K', 1]],
-    'Old_Codegen_Lab': [['L', 1]],
-    'Studio': [['A3', 1]],
-    'Embedded_Lab': [['A4', 1]],
-    'Network_Lab': [['A4', 1]],
-    'Seminar_Room': [['M', 1]],
-    'E_Wis_Lab': [['M', 1]],
-    'Codegen_Lab': [['N', 1]],
-    'Sysco_Lounge': [['O', 1]],
-    'Insight_Hub': [['P', 30], ['Q', 30]],
-    'HPC_Lab': [['S', 1]],
-    'IntelliSense_Lab': [['T', 1]],
-    'L1_Lab': [['U', 1]],
-    'CSE_Office': [['V', 1]],
-    'HoD_Office': [['W', 1]]
+mapping = {
+    "Final_Year_Lab": ['K'],
+    "L1_Lab": ['K', 'L'],
+    "IntelliSense_Lab": ['M'],
+    "HPC_Lab": ['N'],
+    "Embedded_Lab": ['O'],
+    "Network_Lab": ['O'],
+    "Insight_Hub": ['P'],
+    "Sysco_Lounge": ['Q'],
+    "Codegen_Lab": ['R'],
+    "E_Wis_Lab": ['S'],
+    "Seminar_Room": ['S'],
+    "CIT_Studio": ['e'],
+    "Studio": ['d'],
+    "Old_Codegen_Lab": ['A'],
+    "Server_Room": ['c', 'b'],
+    "ICE_Room": ['b', 'B'],
+    "Systems_Lab": ['a'],
+    "Lunch_Room": ['Z'],
+    "Conference_Room": ['B', 'Y'],
+    "Staff_Room_1": ['X', 'D', 'W'],
+    "Staff_Room_2": ['C', 'X', 'Y'],
+    "Old_Advanced_Lab": ['F'],
+    "FYP_Lab": ['G', 'f'],
+    "LSF_Lab": ['f'],
+    "Washroom": ['H'],
+    "Stairs": ['I'],
+    "Fabric_Lab": ['J'],
+    "Instructor_Room": ['J'],
+    "CSE_Office": ['U'],
+    "HoD_Office": ['V'],
 }
 
+graph = {
+    'K': [['J', 5], ['L', 7]],
+    'L': [['K', 7], ['M', 11]],
+    'M': [['L', 11], ['N', 6]],
+    'N': [['M', 6], ['O', 5]],
+    'O': [['N', 5], ['P', 7]],
+    'P': [['O', 7], ['Q', 11], ['U', 14]],
+    'Q': [['P', 11], ['R', 10]],
+    'R': [['Q', 10], ['S', 6]],
+    'S': [['R', 6], ['T', 3]],
+    'T': [['S', 3], ['A', 7], ['d',4]],
+    'd': [['T', 4], ['e', 3]],
+    'e': [['d', 3]],
+    'A': [['T', 7], ['B', 12]],
+    'B': [['A', 12], ['a', 5], ['b', 13], ['C', 15]],
+    'a': [['B', 5], ['b', 5]],
+    'b': [['a', 5], ['B', 13], ['c', 7]],
+    'c': [['b', 7]],
+    'C': [['B', 15], ['D', 13], ['X', 8], ['Y', 10], ['V', 13]],
+    'Y': [['C', 10], ['X', 15], ['Z', 1]],
+    'Z': [['Y', 1]],
+    'X': [['Y', 15], ['C', 8], ['D', 9], ['W', 14]],
+    'D': [['X', 9], ['C', 13], ['W', 11], ['E', 13], ['V', 13]],
+    'W': [['X', 14], ['D', 11], ['E', 9]],
+    'E': [['W', 9], ['F', 7]],
+    'F': [['E', 7], ['G', 13]],
+    'G': [['F', 13], ['f', 15], ['H', 10]],
+    'f': [['G', 15]],
+    'H': [['G', 10], ['I', 3]],
+    'I': [['H', 3], ['J', 7]],
+    'J': [['I', 7], ['K', 5]],
+    'U': [['P', 14], ['V', 8]],
+    'V': [['U', 8], ['D', 13], ['C', 13]],
+}
+
+def get_mapped_node(node):
+    for key, values in mapping.items():
+        if key == node:
+            return values
+
 # Example usage:
-start_node = 'Final_Year_Lab'
-goal_node = 'Server_Room'
-shortest_path = a_star(graph, start_node, goal_node)
+start_node = 'Conference_Room'
+goal_node = 'Sysco_Lounge'
+mapped_start_nodes = get_mapped_node(start_node)
+mapped_goal_nodes = get_mapped_node(goal_node)
+print("Mapped Start Nodes:", mapped_start_nodes, "Mapped Goal Nodes:", mapped_goal_nodes)
+
+
+shortest_path = None
+shortest_distance= float('inf')
+for node in mapped_start_nodes:
+    for goal in mapped_goal_nodes:
+        path, current_distance = a_star(graph, node, goal)
+        print("Distance from", node, "to", mapped_goal_nodes, ":", path)
+        if current_distance < shortest_distance:
+            shortest_path = path
+            shortest_distance = current_distance
+
 print("Shortest Path:", shortest_path)
