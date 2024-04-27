02d77a2286cc3b82894231441586992d0c358230
Nipun <nipun1969@gmail.com>
2024-04-28 00:20:49 +0530

    Add env variables

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import uvicorn
 from controllers.calculateLocation import router as calculateLocation_router
 from controllers.addFingerPrint import router as addFingerPrint_router
 from controllers.healthCheck import router as healthCheck_router
 from controllers.findPath import router as findPath_router
 from db_connect import *
+from dotenv import load_dotenv
+
+load_dotenv()
 
 initialize_db()
 
@@ -14,9 +18,6 @@ app.include_router(calculateLocation_router, prefix="/mylocation", tags=["my-loc
 app.include_router(addFingerPrint_router, prefix="/fingerprint", tags=["finger-print"])
 app.include_router(healthCheck_router, prefix="/health", tags=["health-check"])
 app.include_router(findPath_router, prefix="/path", tags=["path"])
-
-
-
 
 # sample codes 
 
@@ -36,3 +37,7 @@ async def submit_data(req: RequestBody):
     age = req.age
 
     return {"message": f"Received data: Name={name}, Age={age}"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
--- db_connect.py
+++ db_connect.py
@@ -1,8 +1,11 @@
-# db.py
 import mongoengine as me
+import os
 
-# TODO: env file
-connection_string = "mongodb+srv://admin:admin@localization.wyqcqe0.mongodb.net/localization-data?retryWrites=true&w=majority"
+# Now you can access environment variables
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASSWORD")
+
+connection_string = f"mongodb+srv://{db_user}:{db_pass}@localization.wyqcqe0.mongodb.net/localization-data?retryWrites=true&w=majority"
 
 def initialize_db():
     try:
