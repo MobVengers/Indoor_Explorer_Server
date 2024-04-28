fb070a0e99e2c2b9955e61e7e84a39a7c42595db
Udith Kaushalya <udithkaushalya@gmail.com>
2024-04-28 11:15:17 +0530

    added ap and cal finger pr

--- utils/calibrationPoints.py
+++ utils/calibrationPoints.py
@@ -1,19 +1,5 @@
-# from models.calibrationFingerPrint import CalibrationPoint
+from models.calibrationFingerPrint import *
 import uuid
-
-import mongoengine as me
-class Position(me.EmbeddedDocument):
-    x = me.FloatField(required=True)
-    y = me.FloatField(required=True)
-    floor = me.FloatField(required=True)
-
-class CalibrationPoint(me.Document):
-    projectid = me.StringField(required=True)
-    name = me.StringField(required=True)
-    radiomap = me.MapField(field=me.FloatField())  # Map of float values
-    position = me.EmbeddedDocumentField(Position, required=True)
-
-
 
 async def get_calibration_points_by_id(project_id):
     try:
