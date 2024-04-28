20bd96dd4663dc96499e7524294270834cc41189
Udith Kaushalya <udithkaushalya@gmail.com>
2024-04-28 11:04:29 +0530

    db conn issue

--- db_connect.py
+++ db_connect.py
@@ -1,5 +1,5 @@
 import mongoengine as me
-import certifi
+# import certifi
 import os
 
 
@@ -8,8 +8,8 @@ def initialize_db(db_user, db_pass):
     connection_string = f"mongodb+srv://{db_user}:{db_pass}@localization.wyqcqe0.mongodb.net/localization-data?retryWrites=true&w=majority"
 
     try:
-        me.connect(host=connection_string, tlsCAFile=certifi.where())
-        # me.connect(host=connection_string)
+        # me.connect(host=connection_string, tlsCAFile=certifi.where())
+        me.connect(host=connection_string)
         print("## Connected to database")
     except Exception as e:
         print(f"Error connecting to database: {e}")
